af509911a67ee59edf360367b8556e23b5fc2e7e
Michal Kwiatkowski <kfiatos@gmail.com>
2017-04-11 22:43:11 +0200

    Fb php sdk installed

---
+++ src/AppBundle/Controller/FacebookOauthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class FacebookOauthController extends Controller
+{
+    /**
+     * @Route("/facebook/connect", name="facebook_connect")
+     */
+    public function connectAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/facebook/login_check", name="facebook_connect_check")
+     */
+    public function checkAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
