5839351e155337639bbf9581c9bd67745f9392d8
Michal Kwiatkowski <kfiatos@gmail.com>
2017-04-20 18:12:34 +0200

    Fb login partially working

--- src/AppBundle/Controller/FacebookOauthController.php
+++ src/AppBundle/Controller/FacebookOauthController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use Facebook\Facebook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class FacebookOauthController extends Controller
 {
@@ -13,10 +15,34 @@ class FacebookOauthController extends Controller
      */
     public function connectAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+
+        $fb = new Facebook([
+            'app_id' => '680077758842837',
+            'app_secret' => 'c2d1cc0b03fa9eb7c286cda44b1224f9',
+            'default_graph_version' => 'v2.8',
         ]);
+
+
+
+        $helper = $fb->getRedirectLoginHelper();
+        $callbackUrl = $this->generateUrl('facebook_connect_check', [], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $permissions = ['email', 'public_profile']; // Optional permissions
+        $loginUrl = $helper->getLoginUrl($callbackUrl, $permissions);
+
+        return $this->redirect($loginUrl);
+
+
+//        ddd($loginUrl);
+//        die('connect action');
+
+
+//        echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+//
+//        // replace this example code with whatever you need
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+//        ]);
     }
 
     /**
@@ -24,6 +50,14 @@ class FacebookOauthController extends Controller
      */
     public function checkAction(Request $request)
     {
+
+        ddd($request);
+
+        /** @var \GuzzleHttp\Client $client */
+        $client   = $this->get('guzzle.client.api_crm');
+        $response = $client->get();
+        ddd();
+        ddd(json_decode($response->getBody()));
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -25,10 +25,78 @@ class User extends BaseUser
 //     */
 //    private $posts;
 
+    /**
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private $facebookId;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $facebookToken;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $fbTokenExpireTime;
+
     public function __construct()
     {
         parent::__construct();
 //        $this->posts = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
+    /**
+     * @return mixed
+     */
+    public function getFacebookId()
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * @param mixed $facebookId
+     * @return User
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebookId = $facebookId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookToken()
+    {
+        return $this->facebookToken;
+    }
+
+    /**
+     * @param mixed $facebookToken
+     * @return User
+     */
+    public function setFacebookToken($facebookToken)
+    {
+        $this->facebookToken = $facebookToken;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFbTokenExpireTime()
+    {
+        return $this->fbTokenExpireTime;
+    }
+
+    /**
+     * @param mixed $fbTokenExpireTime
+     * @return User
+     */
+    public function setFbTokenExpireTime($fbTokenExpireTime)
+    {
+        $this->fbTokenExpireTime = $fbTokenExpireTime;
+        return $this;
+    }
 }
