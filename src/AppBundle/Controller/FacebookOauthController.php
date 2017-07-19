8b802287e312c52a6a6e34d446e1af31af416434
Michał Kwiatkowski <michal.kwiatkowski@biarchitects.pl>
2017-07-19 15:34:15 +0200

    Simple styling to post index page added

--- src/AppBundle/Controller/FacebookOauthController.php
+++ src/AppBundle/Controller/FacebookOauthController.php
@@ -2,10 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 use Facebook\Facebook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class FacebookOauthController extends Controller
@@ -16,33 +20,16 @@ class FacebookOauthController extends Controller
     public function connectAction(Request $request)
     {
 
-        $fb = new Facebook([
-            'app_id' => '680077758842837',
-            'app_secret' => 'c2d1cc0b03fa9eb7c286cda44b1224f9',
-            'default_graph_version' => 'v2.8',
-        ]);
-
-
-
+        $fb = $this->getFacebook();
         $helper = $fb->getRedirectLoginHelper();
+
+
         $callbackUrl = $this->generateUrl('facebook_connect_check', [], UrlGeneratorInterface::ABSOLUTE_URL);
 
         $permissions = ['email', 'public_profile']; // Optional permissions
         $loginUrl = $helper->getLoginUrl($callbackUrl, $permissions);
 
         return $this->redirect($loginUrl);
-
-
-//        ddd($loginUrl);
-//        die('connect action');
-
-
-//        echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-//
-//        // replace this example code with whatever you need
-//        return $this->render('default/index.html.twig', [
-//            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-//        ]);
     }
 
     /**
@@ -50,17 +37,52 @@ class FacebookOauthController extends Controller
      */
     public function checkAction(Request $request)
     {
+        $fb = $this->getFacebook();
+        $helper = $fb->getRedirectLoginHelper();
+        if (!$accessToken = $helper->getAccessToken()) {
+            //Unauthorized
+            if ($helper->getError()) {
+                //process this error
+            }else {
+                throw New \Exception("Błąd servera");
+            }
+        }
+        if(!$accessToken->isLongLived()) {
+            $oAuth2Client = $fb->getOAuth2Client();
+            try {
+                $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+            } catch(FacebookSDKException $e) {
+                throw new Exception($e->getMessage());
+            }
+        }
 
-        ddd($request);
+        $session = $this->container->get('session');
+        $session->set('fb_access_token', (string) $accessToken);
 
-        /** @var \GuzzleHttp\Client $client */
-        $client   = $this->get('guzzle.client.api_crm');
-        $response = $client->get();
-        ddd();
-        ddd(json_decode($response->getBody()));
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $fb->get('/me?fields=id,name,email', $accessToken);
+        } catch(FacebookResponseException $e) {
+            throw new Exception($e->getMessage());
+        } catch(FacebookSDKException $e) {
+            throw new Exception($e->getMessage());
+        }
+
+        $user = $response->getGraphUser();
+        ddd($user);
+    }
+
+    /**
+     * @return Facebook
+     */
+    public function getFacebook()
+    {
+        $fb = new Facebook([
+            'app_id' => '680077758842837',
+            'app_secret' => 'c2d1cc0b03fa9eb7c286cda44b1224f9',
+            'default_graph_version' => 'v2.8',
         ]);
+        return $fb;
     }
 }
